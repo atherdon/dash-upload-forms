d8bc277a42506bbb7f4100df20b9319e983a79f5
kolserdav <serega12101983@gmail.com>
2021-03-07 18:07:04 +0700

    Change srver.js delete express-favicon

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.NODE_ENV === 'production' ? 5000 : 3000;
 
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico')); 
 
-app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
